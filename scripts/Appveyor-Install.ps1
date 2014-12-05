641e0d4eb858cbecddb8492405164f10ed4c4e8c
Chris Rummel <crummel@microsoft.com>
2014-12-04 18:11:16 -0600

    Add Appveyor settings and install script to fix version numbers.

---
+++ scripts/Appveyor-Install.ps1
@@ -0,0 +1,8 @@
+$regex = New-Object System.Text.RegularExpressions.Regex('<MajorVersion>(\d\.\d)</MajorVersion>')
+$m = $regex.Match((Get-Content citest.props))
+$majorMinor = $m.Groups[1].Value
+$version = $majorMinor + '.' + $env:BuildNumber
+
+Add-AppveyorMessage -Message "Updating version to $version" -Category Information
+
+Update-AppveyorBuild -Version $version
